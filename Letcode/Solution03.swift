4d180bcf462117d15f1ec93e3b78189a2f279523
Cocos <543314602@qq.com>
2020-04-30 23:10:35 +0800

    Update

--- Letcode/Solution03.swift
+++ Letcode/Solution03.swift
@@ -33,13 +33,23 @@ import UIKit
 /// 简单的思路: 因为元素数值不会超过索引范围, 所以直接把数值作为数组下标, 在新创建的数组中, 把对应的位置的内容+1, 如果内容超过2了, 则这个元素就是答案
 class Solution03: NSObject {
     func findRepeatNumber(_ nums: [Int]) -> Int {
-
-        var map = Array(repeating: 0, count: nums.count)
-        for e in nums {
-            if map[e] > 0 {
-                return e
+        // 新的思路, 假设没有数字重复, 那么数字按照顺序放置, 下标为i的位置对应的数字肯定是i
+        // 现在假设元素没有重复, 那么元素i放到位置i去.
+        // 不过题目说了至少有一个数字是重复的, 所以他放到位置i上, 如果i上的元素也是i, 那么i就是重复的
+        // 时间复杂度n, 空间复杂度1
+        var nums = nums
+        for i in 0 ..< nums.count {
+            // i位置的元素a, 应该被安放到下标为a的地方, 如果下标为a的地方刚好也有一个元素是a, 那么这个a就是重复的
+            if nums[i] == i {
+                continue
+            }
+            if nums[i] == nums[nums[i]] {
+                return nums[i]
             }else {
-                map[e] = 1
+                // 把i位置上的元素放到他应该放的位置去
+                let ti = nums[i]
+                nums[i] = nums[ti]
+                nums[ti] = ti
             }
         }
         return -1

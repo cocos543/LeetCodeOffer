a328894c0be68ff6d3fa048358a43dfe01de8bdc
Cocos <543314602@qq.com>
2020-05-09 21:46:14 +0800

    Update

--- Letcode/Solution25.swift
+++ Letcode/Solution25.swift
@@ -21,6 +21,45 @@ import Foundation
  */
 class Solution {
     func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        // 跟合并两个有序数组一样, 主要处理合并后的
+        if l1 == nil {
+            return l2
+        }
+        if l2 == nil {
+            return l1
+        }
+        var head: ListNode?
+        var foot: ListNode?
+        var l1 = l1, l2 = l2
+
+        while l1 != nil && l2 != nil {
+            if l1!.val <= l2!.val {
+                if head == nil {
+                    head = l1
+                }else {
+                    foot!.next = l1
+                }
+                foot = l1
+                l1 = l1!.next
+            }else {
+                if head == nil {
+                    head = l2
+                }else {
+                    foot!.next = l2
+                }
+                foot = l2
+                l2 = l2!.next
+            }
+        }
+        if l1 != nil {
+            foot!.next = l1
+        }else {
+            foot!.next = l2
+        }
+        return head
+    }
+    
+    func mergeTwoLists2(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         // 依次挑选出两条链表里面小的节点, 作为新链表的下一个节点插入到新链表末尾即可
         // 主要最后l1, l2 如果还有剩余的节点, 则一次性插入即可
 

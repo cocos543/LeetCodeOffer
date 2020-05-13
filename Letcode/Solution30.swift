76aad58812c630fc5cd4926bdf89ff7af178e9ce
Cocos <543314602@qq.com>
2020-05-13 21:02:28 +0800

    Update

--- Letcode/Solution30.swift
+++ Letcode/Solution30.swift
@@ -11,6 +11,55 @@ import Foundation
 class MinStack {
 
     var items: [Int]
+    var minStack: [Int] // 辅助栈, 存放当前栈内的最小元素
+
+    /** initialize your data structure here. */
+    init() {
+        self.items = [Int]()
+        self.minStack = [Int]()
+    }
+
+    // 算法思路就是借助一个辅助栈, 存放当前栈内的最小元素.
+    // 如下操作就可以让辅助栈的元素一直是最小的
+    // 1. 存入新元素, 如果等于小于辅助栈顶的, 则放入辅助栈, 这样可以保证辅助栈顶随着元素入栈, 一直是最小的
+    // 2. 元素出栈, 如果和辅助栈顶一样, 则顺便把辅助栈顶元素弹出, 这样可以保证辅助栈的元素一直是有效的.
+    func min() -> Int {
+        if minStack.count == 0 {
+            return -1
+        }
+        return minStack.last!
+    }
+
+    func push(_ ele: Int) {
+        items.append(ele)
+        if minStack.count == 0 || ele <= min() {
+            minStack.append(ele)
+        }
+    }
+
+    func pop() {
+        let ele = items.popLast()
+        if let e = ele {
+            if e == minStack.last! {
+                minStack.popLast()
+            }
+        }
+    }
+
+    func top() -> Int {
+        if items.count == 0 {
+            return -1
+        }else {
+            return items.last!
+        }
+    }
+    
+    
+}
+
+class MinStack2 {
+
+    var items: [Int]
     var minStack: [Int]
 
     /** initialize your data structure here. */

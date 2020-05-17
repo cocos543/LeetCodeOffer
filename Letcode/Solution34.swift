435acb61bb690dd7b3c48986f893c306cea2efc5
Cocos <543314602@qq.com>
2020-05-17 21:06:03 +0800

    Update

--- Letcode/Solution34.swift
+++ Letcode/Solution34.swift
@@ -9,19 +9,46 @@
 import Foundation
 
 class Solution34 {
+    
     func pathSum(_ root: TreeNode?, _ sum: Int) -> [[Int]] {
+        // 采用深度优先搜索的思想, 如果节点的值加起来大于sum则停止继续递归下去.
+        // 如果节点的值加起来刚好等于sum, 而且节点是叶子节点, 那么这条路径就是答案之一
+        if root == nil {
+            return [[Int]]()
+        }
+        var ans = [[Int]]()
+        findPath(root, 0, [Int](), sum, &ans)
+        return ans
+    }
+
+    func findPath(_ root: TreeNode?, _ cur: Int, _ path: [Int], _ sum: Int, _ ans: inout [[Int]]) {
+        if root == nil {
+            return
+        }
+        var path = path
+        path.append(root!.val)
+        if (root!.val + cur) == sum {
+            if root!.left == nil && root!.right == nil {
+                ans.append(path)
+            }
+        }
+        findPath(root!.left, cur + root!.val, path, sum, &ans)
+        findPath(root!.right, cur + root!.val, path, sum, &ans)
+    }
+    
+    func pathSum2(_ root: TreeNode?, _ sum: Int) -> [[Int]] {
 
         // 这道题不需要观察规律, 只要把每一条路径都走一下, 把符合条件的路径保存起来就可以
         var result = [[Int]]()
         
         // 记录走到当前节点的路径
         var path = [Int]()
-        findPath(root, 0, sum, &path, &result)
+        findPath2(root, 0, sum, &path, &result)
 
         return result
     }
     // num: 当前路径的总值
-    func findPath(_ root: TreeNode?, _ num: Int, _ sum: Int, _ path: inout [Int], _ result: inout [[Int]]) {
+    func findPath2(_ root: TreeNode?, _ num: Int, _ sum: Int, _ path: inout [Int], _ result: inout [[Int]]) {
         if root == nil {
             return
         }
@@ -43,10 +70,10 @@ class Solution34 {
         }
 
         if root!.left != nil {
-            findPath(root!.left, num, sum, &path, &result)
+            findPath2(root!.left, num, sum, &path, &result)
         }
         if root!.right != nil {
-            findPath(root!.right, num, sum, &path, &result)
+            findPath2(root!.right, num, sum, &path, &result)
         }
 
     }

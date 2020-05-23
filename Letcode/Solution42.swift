a90972fde46ef21e8f744705e0b7fafe00737b40
Cocos <543314602@qq.com>
2020-05-23 22:20:56 +0800

    update

--- Letcode/Solution42.swift
+++ Letcode/Solution42.swift
@@ -9,7 +9,65 @@
 import Foundation
 
 class Solution42 {
+    
     func maxSubArray(_ nums: [Int]) -> Int {
+        // 复杂度O(n), 说明只能扫一遍数组
+        // 累加每个元素, 如果遇到结果是负数的, 直接丢弃负数的结果, 从下一个元素开始累加.
+        // 累加过程中时时记录历史最大值(哪怕是负数也要记录)
+        if nums.count == 0 {
+            return -2147483648
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+
+        var max = -2147483648
+        var curMax = -2147483648
+        for e in nums {
+            if curMax <= 0 {
+                curMax = e
+            }else {
+                curMax += e
+            }
+            if curMax > max {
+                max = curMax
+            }
+        }
+        return max
+
+    }
+
+
+
+    func maxSubArray2(_ nums: [Int]) -> Int {
+        // 直接一步一步走, 如果curMax是负数的话, 就直接等于下一个数字了, 因为肯定有一个数字是正数, 所以curMax等于下一个数可能就是最大值
+        // 记得每次查找的时候都要记录现有的最大值
+        if nums.count == 0 {
+            return -2147483648
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+
+        var max = -2147483648
+        var curMax = -2147483648
+        for i in 0 ..< nums.count {
+            if curMax <= 0 {
+                // 计算的数组重置成从i开始
+                curMax = nums[i]
+            }else {
+                curMax = curMax + nums[i]
+            }
+            
+            if curMax > max {
+                max = curMax
+            }
+        }
+
+        return max
+    }
+    
+    func maxSubArray2(_ nums: [Int]) -> Int {
         // 直接一步一步走, 如果curMax是负数的话, 就直接等于下一个数字了, 因为肯定有一个数字是正数, 所以curMax等于下一个数可能就是最大值
         // 记得每次查找的时候都要记录现有的最大值
         
--- LetcodeOffer/ViewController.swift
+++ LetcodeOffer/ViewController.swift
@@ -40,14 +40,33 @@ class ViewController: UIViewController {
         let node2 = TreeNode(2)
         let node3 = TreeNode(3)
         let node4 = TreeNode(4)
-        
+
         node1.left = node2
         node1.right = node3
         node2.left = node4
-        
 
         let s = Solution33()
-        print(s.verifyPostorder([4,6,7,5]))
-    
+        print(s.verifyPostorder([4, 6, 7, 5]))
+
+        
+        let nums = [-2,1,-3,4,-1,2,1,-5,4]
+
+        var max = -2147483648
+        var curMax = -2147483648
+        for i in 0 ..< nums.count {
+            if curMax <= 0 {
+                // 计算的数组重置成从i开始
+                curMax = nums[i]
+            }else {
+                curMax = curMax + nums[i]
+            }
+            
+            if curMax > max {
+                max = curMax
+            }
+        }
+
+        print(max)
+        
     }
 }

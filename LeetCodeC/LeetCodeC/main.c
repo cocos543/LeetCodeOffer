8a136b199d071303df1444c2f5875fc75e264ef0
Cocos <543314602@qq.com>
2021-04-22 09:35:36 +0800

    add c project

---
+++ LeetCodeC/LeetCodeC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LeetCodeC
+//
+//  Created by Cocos on 2021/4/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
